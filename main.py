ccaef928f06267593edf3f365331cbb29ec594c7
vincent2021 <vincent2021@gmail.com>
2020-01-14 16:38:34 +0100

    connect to elastic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from formatting.contruct_database import extract_text
 from formatting.contruct_database import launch_preprocessing
 from visualization.tree_graph import render_tree
+from connectdb import upload_to_es
 
 location = os.getcwd() + "/data/data"
 render_tree(location)
